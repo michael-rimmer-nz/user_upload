81f368a043185bf9968842b09ee629c25ea87e82
michael-rimmer-nz <paeinter@windowslive.com>
2018-12-11 20:00:21 +1300

    Removed arguments from being passed in on the script load. This was to simplify the code and make directive & input control more deliberate. Created a user upload class for the addition of private db variables.

--- user_upload.php
+++ user_upload.php
@@ -7,121 +7,8 @@
 
 echo "\n------------user_upload------------\n\n";
 
-$userIsSet = false;
-$passIsSet = false;
-$hostIsSet = false;
-$dbnameIsSet = false;
-
-//defaults
-$host="127.0.0.1"; 
-$user="root"; 
-$password="Password1";
-$dbname="phptest"; 
-
-if ($argc > 1) 
-{	
-	$i = 1;
-	while($i < $argc){
-		if($argv[$i] == '-u') 
-		{
-			echo "-u ";
-			if ($argc > ($i+1)) 
-			{
-				$val = $argv[$i+1];
-				$user = $val;
-				$userIsSet = true;
-				$i = $i + 1;
-			}
-			else
-			{
-				echo "Input Error: Incomplete Argument";
-			}
-		}
-		elseif($argv[$i] == '-p') 
-		{
-			echo "-p ";
-			if ($argc > ($i+1)) 
-			{
-				$val = $argv[$i+1];
-				$password = $val;
-				$passIsSet = true;
-				$i = $i + 1;
-			}
-			else
-			{
-				echo "Input Error: Incomplete Argument";
-			}
-		}	
-		elseif($argv[$i] == '-h') 
-		{
-			echo "-h ";
-			if ($argc > ($i+1)) {
-				$val = $argv[$i+1];
-				$host = $val;
-				$hostIsSet = true;
-				$i = $i + 1;
-			}
-			else
-			{
-				echo "Input Error: Incomplete Argument";
-			}
-		}		
-		elseif($argv[$i] == '-d') 
-		{
-			echo "-d ";
-			if ($argc > ($i+1)) {
-				$val = $argv[$i+1];
-				$dbname= $val;
-				$dbnameIsSet = true;
-				$i = $i + 1;
-			}
-			else
-			{
-				echo "Input Error: Incomplete Argument";
-			}
-		}				
-		$i = $i + 1;
-	}
-	echo "\n";
-	
-	$con = completeUserDatabaseInputs($userIsSet, $passIsSet, $hostIsSet, $dbnameIsSet, $host, $user, $password, $dbname);
-
-		
-	//Display Directive Menu 
-	menu($con);
-} 
-else 
-{
-	echo "No Database arguments passed.\n";
-	echo "Type: 'default' to connect with default database params. \n";
-	echo "Type: 'new' to enter new database params. \n";
-	$handle = fopen ("php://stdin","r");
-	$line = fgets($handle);
-	
-	if(trim($line) == 'default'){
-
-		$con = connectToDatabase($host, $user, $password, $dbname);
-		
-		//Display Directive Menu 
-		menu($con);
-		
-	}else if(trim($line) == 'new'){
-		
-		$con =completeUserDatabaseInputs($userIsSet, $passIsSet, $hostIsSet, $dbnameIsSet, $host, $user, $password, $dbname);
-		 
-		
-		//Display Directive Menu 
-		menu($con);
-		
-	}else{
-		echo "Input Error: Exiting";
-		exit;
-	}
-	
-	fclose($handle);
-}
-
-
+$user_uploader = new UserUpload();
+$user_uploader->menu();
  
 echo "\n\n------------Finish------------\n";
 
@@ -129,188 +16,212 @@ echo "\n\n------------Finish------------\n";
 *	
 *
 */
-function menu($con){
+
+class UserUpload
+{
+	//Database defaults
+	private	$host="127.0.0.1"; 
+	private	$user="root"; 
+	private $password="Password1";
+	private	$dbname="phptest";
+	private	$port=3306;	
 	
-	echo "Enter Commands: \n";
-	$handle = fopen ("php://stdin","r");
-	$line = fgets($handle);
-	$line_arr = explode(" ", $line);
-	fclose($handle);
-	
-	if( sizeof($line_arr) > 0){
+	/**
+	* 		Execution rules: 
+	*		1. Will only execute --[command] per input line.
+	*		2. Will execute multiple -[command] per input line.
+	*													
+	*		--create_table -u [USER] -p [PASSWORD] -d [DBNAME] -pt [PORT]		Creates table to provided database destination. Requires --filename. Should update any existing table.
+	*		--create_table														Creates table using default database params. Requires --filename. Should update any existing table.
+	*		--file [FNAME] e.g --file users.csv									Saves filename to instance
+	*		-u [USER] -p [PASSWORD] -d [DBNAME] -pt [PORT]						Sets Database parameters. Otherwise Default is set. 
+	*		--help																Displays commands
+	*		--dry_run															Creates Table but only prints sanitized values
+	*
+	*/
+	function menu(){
 		
-		echo "\n";
-		$command = trim($line_arr[0]);
+		echo "**Current Database Parameters:** \n";
+		echo "**HOST: $this->host,	 USER: $this->user, 	DBNAME: $this->dbname, 	PORT: $this->port ** \n\n";
 		
-		if(strcmp($command,"--file") == 0)
-		{
+		echo "Enter Commands: \n";
+		$handle = fopen ("php://stdin","r");
+		$line = fgets($handle);
+		$line_arr = explode(" ", $line);
+		fclose($handle);
+		
+		if( sizeof($line_arr) > 0){
 			
-		}
-		elseif(strcmp($command,"--create_table") == 0)
-		{
-			if( sizeof($line_arr) > 1){
+			$i = 0;
+			while($i < sizeof($line_arr)){
+			
+				echo "\n";
+				$command = trim($line_arr[$i]);
 				
-				$i = 1;
-				while($i < sizeof($line_arr))
-				{
-					if(strcmp(trim($line_arr[$i]),"--file") == 0)
+				//TODO: add if starts with '--' and line_arr > 1 then error with 'one command per entry'. 
+				
+				try {
+					if(strcmp($command,"--file") == 0)
 					{
 						
-					}					
+					}
+					elseif(strcmp($command,"--create_table") == 0)
+					{
+		
+						$this->connectToDatabase();
+						//createTable($con);
+						echo "\n";
+					}			
+					elseif(strcmp($command,"--dry_run") == 0)
+					{
+
+					}			
+					elseif(strcmp($command,"-u") == 0)
+					{
+						//Set User
+						echo "\n";
+					}				
+					elseif(strcmp($command,"-p") == 0)
+					{
+						//Set Password
+						echo "\n";
+					}			
+					elseif(strcmp($command,"-h") == 0)
+					{
+						//Set Host
+						echo "\n";
+					}			
+					elseif(strcmp($command,"--help") == 0)
+					{
+						//Display Directives and Descriptions
+						echo "\n";
+					}		
+					elseif(strcmp($command,"--exit") == 0 || strcmp($command,"x") == 0)
+					{
+						echo "Exiting \n";
+						exit;
+					}
+					else
+					{
+						throw new InvalidArgumentException("Incorrect Input");
+					}
 				}
+				catch (Exception $e){
+					echo 'Caught exception: ',  $e->getMessage(), "\n";
+				}
+				
+				$i = $i +1;
 			}
+			//loop
+			$this->menu();
+		}
+		echo "input passed";	
+	}
+
+	function connectToDatabase()
+	{
+		$port=3306;
+		$socket="";
+
+		$con = new mysqli($this->host, $this->user, $this->password, $this->dbname, $this->port, $socket)
+			or die ('Could not connect to the database server' . mysqli_connect_error());
 			
-			createTable($con);
-			echo "\n";
-		}			
-		elseif(strcmp($command,"--dry_run") == 0)
+		echo "connecton established \n";
+
+		return $con;
+	}
+
+
+	/**
+	*/
+	function completeUserDatabaseInputs($userIsSet, $passIsSet, $hostIsSet, $dbnameIsSet, $host, $user, $password, $dbname){
+
+		if(!$userIsSet)
 		{
-			//Request file input
-			echo "Enter File: \n";	
+			echo "Type: 'Default' to connect with default user name: 'root'. \n";
+			echo "Type: 'new' to enter new user name. \n";
 			$handle = fopen ("php://stdin","r");
 			$line = fgets($handle);
-			$fname =  trim($line);
 			fclose($handle);
-		}			
-		elseif(strcmp($command,"-u") == 0)
-		{
-			//Set User
-			echo "\n";
-		}				
-		elseif(strcmp($command,"-p") == 0)
-		{
-			//Set Password
-			echo "\n";
-		}			
-		elseif(strcmp($command,"-h") == 0)
-		{
-			//Set Host
-			echo "\n";
-		}			
-		elseif(strcmp($command,"--help") == 0)
-		{
-			//Display Directives and Descriptions
-			echo "\n";
-		}		
-		elseif(strcmp($command,"--exit") == 0 || strcmp($command,"x") == 0)
-		{
-			echo "Exiting \n";
-			exit;
+			
+			if(trim($line) == 'new'){
+				echo "Enter new value: \n";
+				$handle = fopen ("php://stdin","r");
+				$val = fgets($handle);
+				$user = $val;
+			}else{
+				echo "Using default value \n";
+			}
 		}
-		else
+		if(!$passIsSet)
 		{
-			echo "Incorrect Input. \n";
-		}
-		
-		//loop
-		menu();
-	}
-	echo "input passed";	
-}
-
-function connectToDatabase($host, $user, $password, $dbname)
-{
-	$port=3306;
-	$socket="";
-
-	$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-		or die ('Could not connect to the database server' . mysqli_connect_error());
-		
-	echo "connecton established \n";
-
-	return $con;
-}
-
-
-/**
-*/
-function completeUserDatabaseInputs($userIsSet, $passIsSet, $hostIsSet, $dbnameIsSet, $host, $user, $password, $dbname){
-
-	if(!$userIsSet)
-	{
-		echo "Type: 'Default' to connect with default user name: 'root'. \n";
-		echo "Type: 'new' to enter new user name. \n";
-		$handle = fopen ("php://stdin","r");
-		$line = fgets($handle);
-		fclose($handle);
-		
-		if(trim($line) == 'new'){
-			echo "Enter new value: \n";
+			echo "Type: 'Default' to connect with default password. \n";
+			echo "Type: 'new' to enter new password. \n";
 			$handle = fopen ("php://stdin","r");
-			$val = fgets($handle);
-			$user = $val;
-		}else{
-			echo "Using default value \n";
+			$line = fgets($handle);
+			fclose($handle);
+			if(trim($line) == 'new'){
+				echo "Enter new value: \n";
+				$handle = fopen ("php://stdin","r");
+				$val = fgets($handle);
+				$password = $val;
+			}else{
+				echo "Using default value \n";
+			}
 		}
-	}
-	if(!$passIsSet)
-	{
-		echo "Type: 'Default' to connect with default password. \n";
-		echo "Type: 'new' to enter new password. \n";
-		$handle = fopen ("php://stdin","r");
-		$line = fgets($handle);
-		fclose($handle);
-		if(trim($line) == 'new'){
-			echo "Enter new value: \n";
+		if(!$hostIsSet)
+		{
+			echo "Type: 'Default' to connect with default host: 127.0.0.1  \n";
+			echo "Type: 'new' to enter new host. \n";
 			$handle = fopen ("php://stdin","r");
-			$val = fgets($handle);
-			$password = $val;
-		}else{
-			echo "Using default value \n";
+			$line = fgets($handle);
+			fclose($handle);
+			if(trim($line) == 'new'){
+				echo "Enter new value: \n";
+				$handle = fopen ("php://stdin","r");
+				$val = fgets($handle);
+				$host = $val;
+			}else{
+				echo "Using default value \n";
+			}
 		}
-	}
-	if(!$hostIsSet)
-	{
-		echo "Type: 'Default' to connect with default host: 127.0.0.1  \n";
-		echo "Type: 'new' to enter new host. \n";
-		$handle = fopen ("php://stdin","r");
-		$line = fgets($handle);
-		fclose($handle);
-		if(trim($line) == 'new'){
-			echo "Enter new value: \n";
+		if(!$dbnameIsSet)
+		{
+			echo "Type: 'Default' to connect with default Database Name: 'phptest'  \n";
+			echo "Type: 'new' to enter Database Name. \n";
 			$handle = fopen ("php://stdin","r");
-			$val = fgets($handle);
-			$host = $val;
-		}else{
-			echo "Using default value \n";
+			$line = fgets($handle);
+			fclose($handle);
+			if(trim($line) == 'new'){
+				echo "Enter new value: \n";
+				$handle = fopen ("php://stdin","r");
+				$val = fgets($handle);
+				$dbname = $val;
+			}else{
+				echo "Using default value \n";
+			}
 		}
-	}
-	if(!$dbnameIsSet)
-	{
-		echo "Type: 'Default' to connect with default Database Name: 'phptest'  \n";
-		echo "Type: 'new' to enter Database Name. \n";
-		$handle = fopen ("php://stdin","r");
-		$line = fgets($handle);
-		fclose($handle);
-		if(trim($line) == 'new'){
-			echo "Enter new value: \n";
-			$handle = fopen ("php://stdin","r");
-			$val = fgets($handle);
-			$dbname = $val;
-		}else{
-			echo "Using default value \n";
-		}
+
+		return connectToDatabase($host, $user, $password, $dbname);
 	}
 
-	return connectToDatabase($host, $user, $password, $dbname);
-}
 
+	function createTable($con)
+	{
+		$sql = "CREATE TABLE users (
+				email VARCHAR(50) PRIMARY KEY, 
+				name VARCHAR(30) NOT NULL,
+				surname VARCHAR(30) NOT NULL
+				)";
+			
+		if (mysqli_query($con, $sql)) {
+			echo "Table Users created successfully";
+		} else {
+			echo "Error creating table: " . mysqli_error($con);
+		}
 
-function createTable($con)
-{
-	$sql = "CREATE TABLE users (
-			email VARCHAR(50) PRIMARY KEY, 
-			name VARCHAR(30) NOT NULL,
-			surname VARCHAR(30) NOT NULL
-			)";
+		mysqli_close($con);
 		
-	if (mysqli_query($con, $sql)) {
-		echo "Table Users created successfully";
-	} else {
-		echo "Error creating table: " . mysqli_error($con);
+		
 	}
-
-	mysqli_close($con);
-	
-	
 }
