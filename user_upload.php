2157fc26574dc3add513bb6a0e961560c435dd1d
michael-rimmer-nz <paeinter@windowslive.com>
2018-12-12 21:00:28 +1300

    Completed basic functionality. Added help display. Added table and entry exist checks. Completed --dry_run and create table. Working on local PC.

--- user_upload.php
+++ user_upload.php
@@ -8,6 +8,7 @@
 echo "\n------------user_upload------------\n\n";
 
 $user_uploader = new UserUpload();
+$user_uploader->init();
 $user_uploader->menu();
  
 echo "\n\n------------Finish------------\n";
@@ -30,6 +31,15 @@ class UserUpload
 	private $fnameIsSet = false;
 	
 	/**
+	*
+	*/
+	function init(){
+			define("NAME", 0);
+			define("SURNAME", 1);
+			define("EMAIL", 2);
+	}
+	
+	/**
 	* 		Execution rules: 
 	*		1. Will only execute --[command] per input line.
 	*		2. Will execute multiple -[command] per input line.
@@ -46,6 +56,14 @@ class UserUpload
 		
 		echo "\n**Current Database Parameters:** \n";
 		echo "**HOST: $this->host,	 USER: $this->user, 	DBNAME: $this->dbname, 	PORT: $this->port ** \n\n";
+		if($this->fnameIsSet)
+		{
+			echo "**File Path: $this->fname ** \n\n";
+		}
+		else
+		{
+			echo "**File Path: Unset ** \n\n";
+		}
 		
 		echo "Enter Commands: \n";
 		$handle = fopen ("php://stdin","r");
@@ -56,6 +74,9 @@ class UserUpload
 		if( sizeof($line_arr) > 0){
 			
 			$i = 0;
+			$creatingTable = false;
+			$creatingTableSQL = false;
+			
 			while($i < sizeof($line_arr)){
 			
 				echo "\n";
@@ -71,32 +92,34 @@ class UserUpload
 							$val =  trim($line_arr[$i+1]);
 							$val = strtolower($val);
 							
-							if(preg_match('/^[a-z0-9-.]+$/', $val)) 
+							if (file_exists($val))
 							{
+								echo "File found. File path set.\n";
 								$this->fname = $val;	
 								$this->fnameIsSet = true;
 								$i = $i +1;	
 							} 
 							else 
 							{
-								throw new InvalidArgumentException("Incorrect File Input");
+								$i = $i +1;	
+								throw new InvalidArgumentException("Incorrect file path input");
 							}
 						}
 						else{
-							throw new InvalidArgumentException("Incorrect File Input");
+							throw new InvalidArgumentException("No file path provided.");
 						}
-						
 					}
 					elseif(strcmp($command,"--create_table") == 0)
 					{
-
-						$this->connectToDatabase();
-						//createTable($con);
-						echo "\n";
+						//defer create table until line is fully read. 
+						$creatingTable = true; 
+						$creatingTableSQL = true; 
 					}			
 					elseif(strcmp($command,"--dry_run") == 0)
 					{
-
+						//defer create table until line is fully read. 
+						$creatingTable = true; 
+						$creatingTableSQL = false; 
 					}			
 					elseif(strcmp($command,"-u") == 0)
 					{
@@ -108,8 +131,7 @@ class UserUpload
 						else
 						{
 							$i = $i +1;
-							throw new InvalidArgumentException("Incorrect User Input");
-							
+							throw new InvalidArgumentException("Incorrect User Input");		
 						}
 					}				
 					elseif(strcmp($command,"-p") == 0)
@@ -122,8 +144,7 @@ class UserUpload
 						else
 						{
 							$i = $i +1;
-							throw new InvalidArgumentException("Incorrect Password Input");
-							
+							throw new InvalidArgumentException("Incorrect Password Input");	
 						}
 					}			
 					elseif(strcmp($command,"-h") == 0)
@@ -137,9 +158,7 @@ class UserUpload
 						{
 							$i = $i +1;
 							throw new InvalidArgumentException("Incorrect Host Input");
-							
-						}
-						
+						}	
 					}					
 					elseif(strcmp($command,"-d") == 0)
 					{
@@ -152,9 +171,7 @@ class UserUpload
 						{
 							$i = $i +1;
 							throw new InvalidArgumentException("Incorrect DBname Input");
-							
-						}
-						
+						}	
 					}					
 					elseif(strcmp($command,"-pt") == 0)
 					{
@@ -167,14 +184,14 @@ class UserUpload
 						{
 							$i = $i +1;
 							throw new InvalidArgumentException("Incorrect port Input");
-							
 						}
 					}			
 					elseif(strcmp($command,"--help") == 0)
 					{
 						//Display Directives and Descriptions
+						$this->displayHelp();
 						echo "\n";
-					}		
+					}						
 					elseif(strcmp($command,"--exit") == 0 || strcmp($command,"x") == 0)
 					{
 						echo "Exiting \n";
@@ -188,31 +205,23 @@ class UserUpload
 				catch (Exception $e){
 					echo 'Caught exception: ',  $e->getMessage(), "\n";
 				}
-				
 				$i = $i +1;
 			}
+			
+			if($creatingTable){
+				if(!$this->fnameIsSet){
+					echo "File name needs to be set first. Set it with '--file' command. \n";
+				}else{
+					$this->insertFromFile($creatingTableSQL);
+				}
+				echo "\n";
+			}
+			
 			//loop menu
 			$this->menu();
-		}
-		echo "input passed";	
+		}	
 	}
 
-	/**
-	*
-	*/
-	function connectToDatabase()
-	{
-		$port=3306;
-		$socket="";
-
-		$con = new mysqli($this->host, $this->user, $this->password, $this->dbname, $this->port, $socket)
-			or die ('Could not connect to the database server' . mysqli_connect_error());
-			
-		echo "connecton established \n";
-
-		return $con;
-	}
-	
 	/**
 	*	Looks ahead in the line then reads the input value for single dash '-' inputs. If correct db param is set. 
 	*	Input line index is incremented after read. 
@@ -235,12 +244,9 @@ class UserUpload
 					} 
 					break;
 				case '-p':
-					if(preg_match('/([A-Za-z0-9])+/',$val)) 
-					{
-						$this->password = $val;	
-						echo "New PASSWORD entered \n";
-						return true;
-					} 
+					$this->password = $val;	
+					echo "New PASSWORD entered \n";
+					return true;
 					break;					
 				case '-h':
 					if(filter_var($val, FILTER_VALIDATE_IP)) 
@@ -272,7 +278,64 @@ class UserUpload
 		}	
 		return false;
 	}
+	
+	/**
+	*
+	*
+	*/
+	function displayHelp(){
+		echo "*****Script Command Directives***** \n\n";
+		echo "Command:		Description: \n";
+		echo "--create_table		Creates table using default database params. Requires --file. Should update any existing table.\n";
+		echo "--dry_run		Creates Table printing sanitized values with no table insertion.\n";
+		echo "--file [FNAME]		Set the file path for the input file.\n";
+		echo "-u [USER]		Sets respective database  parameters. Otherwise the default parameter value is set.\n";
+		echo "-p [PASSWORD]		Sets respective database  parameters. Otherwise the default parameter value is set.\n";
+		echo "-d [DBNAME]		Sets respective database  parameters. Otherwise the default parameter value is set.\n";
+		echo "-h [HOST]		Sets respective database  parameters. Otherwise the default parameter value is set.\n";
+		echo "-pt [PORT]		Sets respective database  parameters. Otherwise the default parameter value is set.\n";
+		echo "--help			Displays commands.\n";
+		echo "--exit OR x		Closes script.\n";
+		
+	}
 
+	/**
+	*
+	*/
+	function connectToDatabase()
+	{
+		$port=3306;
+		$socket="";
+
+		$con = new mysqli($this->host, $this->user, $this->password, $this->dbname, $this->port, $socket)
+			or die ('Could not connect to the database server' . mysqli_connect_error());
+			
+		echo "connecton established \n";
+
+		return $con;
+	}
+	
+	function tableExists($con, $table){
+		if ($result = $con->query("SHOW TABLES LIKE '".$table."'")) {
+			if($result->num_rows == 1) {
+				return true;
+			}
+		}
+		else {
+			return false;
+		}	
+	}
+	
+	function entryExists($con, $email){
+		if ($result = $con->query("SELECT * FROM users WHERE email='".$email."'")) {
+			if($result->num_rows > 0) {
+				return true;
+			}
+		}
+		else {
+			return false;
+		}
+	}
 
 	function createTable($con)
 	{
@@ -293,15 +356,31 @@ class UserUpload
 		
 	}
 	
-	function insertFromFile($con){
+	function insertFromFile($updateSQLTable){
 	
 		$row = 1;
-		if (($handle = fopen("users.csv", "r")) !== FALSE) 
+		$con = NULL;
+		
+		if (($handle = fopen($this->fname, "r")) !== FALSE) 
 		{
 			
 			$col_num = 0; 
 			$columns = "";
 			$email_arr = array();
+			
+			//if updateSQLTable connect to db
+			if($updateSQLTable)
+			{
+				$con = $this->connectToDatabase();
+				if($this->tableExists($con, 'users'))
+				{
+					echo "Database has table.\n";
+				}
+				else
+				{
+					$this->createTable($con);
+				}
+			}
 			
 			while (($data = fgetcsv($handle, 1000, ",")) !== FALSE)
 			{
@@ -317,6 +396,7 @@ class UserUpload
 					$value_num = count($data);
 					$values_arr = array();
 					$valid_line = false;
+					$entry_email = "";
 					
 					if($value_num == $col_num){
 						for ($i=0; $i < $value_num; $i++) 
@@ -325,19 +405,19 @@ class UserUpload
 							$str = strtolower($str);
 											
 							switch ($i) {
-								case 0:
+								case NAME:
 									$str = preg_replace("/[^A-Za-z0-9\-']/", "", $str);
 									$str = str_replace("'", "''", $str);
 									$str = "'".ucfirst($str)."'";
 									array_push($values_arr,$str);
 									break;
-								case 1:
+								case SURNAME:
 									$str = preg_replace("/[^A-Za-z0-9\-']/", "", $str);
 									$str = str_replace("'", "''", $str);
 									$str = "'".ucfirst($str)."'";
 									array_push($values_arr,$str);
 									break;
-								case 2:
+								case EMAIL:
 									if (!filter_var($str, FILTER_VALIDATE_EMAIL) === false) 
 									{
 										$valid_line = true;
@@ -346,6 +426,7 @@ class UserUpload
 										if(!in_array($str, $email_arr))
 										{
 											array_push($email_arr,$str);
+											$entry_email = $str;
 										}
 										else
 										{
@@ -374,16 +455,27 @@ class UserUpload
 						
 						if($valid_line)
 						{
-							echo "\n";
-							$sql = "INSERT INTO users ($columns) VALUES ($values); ";
-							echo $sql;
-							echo "\n";
-						
-							if (mysqli_query($con, $sql)) {
-								echo "Table Users Insertion Success";
-							} else {
-								echo "Insertion Error: " . mysqli_error($con);
-							}					
+							if($updateSQLTable){
+								
+								$sql = "";
+								//check entryExists if exist use update SQL 
+								if($this->entryExists($con, $entry_email)){
+									$sql = "REPLACE INTO users ($columns) VALUES ($values);";
+								}else{
+									$sql = "INSERT INTO users ($columns) VALUES ($values); ";
+								}
+								echo "\n$sql \n";
+								if (mysqli_query($con, $sql)) {
+									echo "Table Users Insertion Success";
+								} else {
+									echo "Insertion Error: " . mysqli_error($con);
+								}	
+							}else{
+				
+								$sql = "INSERT INTO users ($columns) VALUES ($values);";
+								echo "\n$sql \n";
+							
+							}							
 						} 
 					}
 					else
@@ -395,7 +487,7 @@ class UserUpload
 				
 				$row++;	
 			}
-			mysqli_close($con);
+			if($updateSQLTable){mysqli_close($con);}
 			fclose($handle);
 		}
 		echo "\n";
